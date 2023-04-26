a344d7d608a1f8a921d5cc25bd82c2aca44c13b4
Giulia Testa <giuliatestax@gmail.com>
2023-04-26 16:03:52 +0200

    Initial Commit

---
+++ udriveios/udriveiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  udriveiosApp.swift
+//  udriveios
+//
+//  Created by Giulia Testa on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct udriveiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
