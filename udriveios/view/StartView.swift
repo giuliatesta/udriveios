5710fd33e5f0267f15b62144b7bc75de116becd6
Giulia Testa <giuliatestax@gmail.com>
2023-07-11 17:09:01 +0200

    initial draft request location authorization before first start

--- udriveios/view/StartView.swift
+++ udriveios/view/StartView.swift
@@ -6,18 +6,51 @@
 //
 
 import SwiftUI
+import CoreLocation
+
+func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) -> Bool {
+    switch manager.authorizationStatus {
+    case .authorizedWhenInUse:  // Location services are available.
+        //enableLocationFeatures()
+        break
+    case .restricted, .denied:  // Location services currently unavailable.
+        //disableLocationFeatures()
+        break
+    case .notDetermined:        // Authorization not determined yet.
+        manager.requestWhenInUseAuthorization()
+        break
+    default:
+        break
+    }
+    return manager.authorizationStatus == .authorizedWhenInUse || manager.authorizationStatus == .authorizedAlways
+}
+
 
 struct StartView: View {
+    @State private var showStopAlert = false;
+
+    @State private var locationAuthorized : Bool = false;
+    
     var body: some View {
         
         NavigationView{
             VStack{
                 Text("Rotate your phone vertically").font(fontSystem)
                 GifImage("rotate_phone").frame(width: 150, height: 150, alignment: .center)
-                NavigationLink(destination: HomePage(), label: {
-                    Text("Start Driving!").font(.largeTitle)
-                })
+                    Button(action: {
+                        let locationManager = CLLocationManager();
+                        locationAuthorized = locationManagerDidChangeAuthorization(locationManager)
+                        print(locationAuthorized)
+                    })
+                    {
+                        Text("Start Driving!")
+                    }
                 .padding()
+                NavigationLink(destination: HomePage(),
+                    isActive: $locationAuthorized
+                ){
+                    EmptyView()
+                }
             }
         }
         .navigationBarTitle("")
